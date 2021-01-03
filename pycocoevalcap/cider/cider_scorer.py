2e42a428cd90f1c40bfeb88b37468e65338796d5
yuyao <yukyewshek@outlook.com>
2021-01-03 22:31:24 +0100

    modify line 130&161, use update iteritems() to items()

--- pycocoevalcap/cider/cider_scorer.py
+++ pycocoevalcap/cider/cider_scorer.py
@@ -16,6 +16,7 @@ import numpy as np
 import math
 import os
 
+
 def precook(s, n=4, out=False):
     """
     Takes a string as input and returns an object that can be given to
@@ -27,13 +28,14 @@ def precook(s, n=4, out=False):
     """
     words = s.split()
     counts = defaultdict(int)
-    for k in range(1,n+1):
-        for i in range(len(words)-k+1):
-            ngram = tuple(words[i:i+k])
+    for k in range(1, n + 1):
+        for i in range(len(words) - k + 1):
+            ngram = tuple(words[i:i + k])
             counts[ngram] += 1
     return counts
 
-def cook_refs(refs, n=4): ## lhuang: oracle will call with "average"
+
+def cook_refs(refs, n=4):  ## lhuang: oracle will call with "average"
     '''Takes a list of reference sentences for a single segment
     and returns an object that encapsulates everything that BLEU
     needs to know about them.
@@ -43,6 +45,7 @@ def cook_refs(refs, n=4): ## lhuang: oracle will call with "average"
     '''
     return [precook(ref, n) for ref in refs]
 
+
 def cook_test(test, n=4):
     '''Takes a test sentence and returns an object that
     encapsulates everything that BLEU needs to know about it.
@@ -51,6 +54,7 @@ def cook_test(test, n=4):
     :return: result (dict)
     '''
     return precook(test, n, True)
+
 
 class CiderScorer(object):
     """CIDEr scorer.
@@ -79,9 +83,9 @@ class CiderScorer(object):
         if refs is not None:
             self.crefs.append(cook_refs(refs))
             if test is not None:
-                self.ctest.append(cook_test(test)) ## N.B.: -1
+                self.ctest.append(cook_test(test))  ## N.B.: -1
             else:
-                self.ctest.append(None) # lens of crefs and ctest have to match
+                self.ctest.append(None)  # lens of crefs and ctest have to match
 
     def size(self):
         assert len(self.crefs) == len(self.ctest), "refs/test mismatch! %d<>%d" % (len(self.crefs), len(self.ctest))
@@ -108,10 +112,10 @@ class CiderScorer(object):
         '''
         for refs in self.crefs:
             # refs, k ref captions of one image
-            for ngram in set([ngram for ref in refs for (ngram,count) in ref.iteritems()]):
+            for ngram in set([ngram for ref in refs for (ngram, count) in ref.iteritems()]):
                 self.document_frequency[ngram] += 1
 
-    def compute_cider(self, df_mode = "corpus"):
+    def compute_cider(self, df_mode="corpus"):
         def counts2vec(cnts):
             """
             Function maps counts of ngram to vector of tfidf weights.
@@ -123,13 +127,13 @@ class CiderScorer(object):
             vec = [defaultdict(float) for _ in range(self.n)]
             length = 0
             norm = [0.0 for _ in range(self.n)]
-            for (ngram,term_freq) in cnts.iteritems():
+            for (ngram, term_freq) in cnts.items():
                 # give word count 1 if it doesn't appear in reference corpus
                 df = np.log(max(1.0, self.document_frequency[ngram]))
                 # ngram index
-                n = len(ngram)-1
+                n = len(ngram) - 1
                 # tf (term_freq) * idf (precomputed idf) for n-grams
-                vec[n][ngram] = float(term_freq)*(self.ref_len - df)
+                vec[n][ngram] = float(term_freq) * (self.ref_len - df)
                 # compute norm for the vector.  the norm will be used for computing similarity
                 norm[n] += pow(vec[n][ngram], 2)
 
@@ -154,16 +158,16 @@ class CiderScorer(object):
             val = np.array([0.0 for _ in range(self.n)])
             for n in range(self.n):
                 # ngram
-                for (ngram,count) in vec_hyp[n].iteritems():
+                for (ngram, count) in vec_hyp[n].items():
                     # vrama91 : added clipping
                     val[n] += min(vec_hyp[n][ngram], vec_ref[n][ngram]) * vec_ref[n][ngram]
 
                 if (norm_hyp[n] != 0) and (norm_ref[n] != 0):
-                    val[n] /= (norm_hyp[n]*norm_ref[n])
+                    val[n] /= (norm_hyp[n] * norm_ref[n])
 
-                assert(not math.isnan(val[n]))
+                assert (not math.isnan(val[n]))
                 # vrama91: added a length based gaussian penalty
-                val[n] *= np.e**(-(delta**2)/(2*self.sigma**2))
+                val[n] *= np.e ** (-(delta ** 2) / (2 * self.sigma ** 2))
             return val
 
         # compute log reference length
@@ -194,12 +198,12 @@ class CiderScorer(object):
         if df_mode == "corpus":
             self.compute_doc_freq()
             # assert to check document frequency
-            assert(len(self.ctest) >= max(self.document_frequency.values()))
+            assert (len(self.ctest) >= max(self.document_frequency.values()))
         else:
             docFreq = df_file
-            self.document_frequency = docFreq['df']
+            self.document_frequency = docFreq[b'df']
             # TODO: make this a part of coco-val-df
-            self.ref_len = np.log(float(docFreq['ref_len']))
+            self.ref_len = np.log(float(docFreq[b'ref_len']))
 
         # compute cider score
         score = self.compute_cider(df_mode)
